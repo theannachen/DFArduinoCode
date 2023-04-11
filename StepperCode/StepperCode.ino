4aed6d0a07f066f0a75fc528c0b714a549e93377
theannachen <annachen031014@gmail.com>
2023-04-11 10:33:20 -0500

    commented wiring for arduino for constants

--- StepperCode/StepperCode.ino
+++ StepperCode/StepperCode.ino
@@ -2,13 +2,20 @@
 
 AccelStepper stepper(AccelStepper::DRIVER, 3, 2);
 
+//max is 4000 steps per second
+
+//LED: Digital Pins at 3, 5, 6
+
+//Stepper Driver 8, 9
+
+//LCD 4, 6, 10, 11, 12, 13
+
 void setup() {
   // put your setup code here, to run once:
   stepper.setMaxSpeed(1000);
   stepper.setSpeed(50);
 
 }
-#hi!
 
 void loop() {
   // put your main code here, to run repeatedly:
