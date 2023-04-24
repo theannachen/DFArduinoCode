89a0bbda7e8bf5db55f02a7259e40709957dd1eb
David Perez <david.a.perez@vanderbilt.edu>
2023-04-24 16:20:16 -0500

    Fix compilation issues and rewrite logic

--- StepperCode/StepperCode.ino
+++ StepperCode/StepperCode.ino
@@ -2,37 +2,48 @@
 // 4000 steps/s max
 // 200 steps in rotation
 
+// Lead Screw Notes
+// Each rotation is 0.2 cm
 
-//Pin Notes
-//22 and up are all digital
-//2-13 are PWM
-
-
+// Pin Notes
+// 22 and up are all digital
+// 2-13 are PWM
 
 #include <AccelStepper.h>
 #include <LiquidCrystal.h>
 #include <Math.h>
 
-//Set Geometry of Syringe
+// Geometry of Syringe
 const float radiusMm = 5.0;
-const float lengthMm = 20.0;
-const int startingMillis = 20;
 
-//Milliliters per minute
+// Milliliters per minute
 const float flowRateMinutes = 3.0;
 
-//Milliliters per second
-const flowRate = flowRateMinutes/60;
+// Milliliters per second
+const float flowRate = flowRateMinutes/60;
 
-//LCD Pins
+const int maxSpeed = 4000;
+
+const float cmPerRevolution = 0.2;
+
+const float stepsPerRevolution = 200.0;
+
+const float surfaceArea = (sq(radiusMm) * M_PI);
+
+// cm/s
+const float linearSpeed = flowRate / surfaceArea;
+
+// steps/s
+const int stepSpeed = int(linearSpeed / cmPerRevolution * stepsPerRevolution);
+// LCD Pins
 const int rs = 42, en = 44, d4 = 46, d5 = 48, d6 = 50, d7 = 52;
 
-//Motor Bins
-const int stepPin = 2, motorDirection = 22;
+// Motor Pins
+const int stepPin = 2, directionPin = 22;
 
-const int[3] red = [255, 0 , 0];
-const int[3] yellow = [255, 255, 0];
-const int[3] green = [0, 0, 255];
+const int red[3] = { 255, 0 , 0 };
+const int yellow[3] = { 255, 255, 0 };
+const int green[3] = { 0, 255, 0 };
 
 //Buttons + Limit switch
 const int mb =  24, fb = 28, bb = 30, ls = 26;
@@ -40,20 +51,19 @@ const int mb =  24, fb = 28, bb = 30, ls = 26;
 //RGB Components
 const int rp = 3, gp = 4, bp = 5;
 
-//TODO: How to calculate max speed
-const int maxSpeed = 40;
 
-//Setup Libraries
+
+// Setup Libraries
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-AccelStepper stepper(AccelSteppere::DRIVER, motorDirection, stepPin);
+AccelStepper stepper(AccelStepper::DRIVER, stepPin, directionPin);
 
 void setup() {
   Serial.begin(9600);
 
   pinMode(stepPin, OUTPUT);
-  pinMode(motorDirection, OUTPUT);
+  pinMode(directionPin, OUTPUT);
 
-  //configure input pins and enable the internal pull-up resistor
+  //Configure input pins and enable the internal pull-up resistor
   pinMode(mb, INPUT_PULLUP);
   pinMode(fb, INPUT_PULLUP);
   pinMode(bb, INPUT_PULLUP);
@@ -63,7 +73,6 @@ void setup() {
   pinMode(gp, OUTPUT);
   pinMode(bp, OUTPUT);
 
-  //Show flow rate and time remaining
   lcd.begin(16, 2);
 
   pinMode(rs, OUTPUT);
@@ -73,18 +82,8 @@ void setup() {
   pinMode(d6, OUTPUT);
   pinMode(d7, OUTPUT);
 
-  int running = 0;
-
-  float surfaceArea = (sq(radiusMm) * M_PI);
-
-  //cm/s
-  float speed = flowrate / surfaceArea;
-
-  //steps/s
-  int stepSpeed = int(speed * 8 * 200);
-
   stepper.setMaxSpeed(maxSpeed);
-  stepper.setSpeed(min(speed, maxSpeed));
+  stepper.setSpeed(linearSpeed);
 
   lcd.setCursor(0,0);
   lcd.print(String(flowRate, 3));
@@ -92,9 +91,6 @@ void setup() {
 
   lcd.setCursor(0,1);
   lcd.print("Time Left: ");
-
-  int totalMillis = (startingMillis / flowRate) * 1000;
-  int timeLeft = 0;
 }
 
 void loop() {
@@ -110,54 +106,42 @@ void loop() {
   int backVal = digitalRead(bb);
   int limitVal = digitalRead(ls);
   
-  if (ls == HIGH){
-
-    if (mainVal == HIGH) {
-      stepper.runSpeed();
-      if (running == 0){
-        int startTime = millis();
-        int endTime = startTime + totalMillis;
-        running = 1;
-      }
-
-      timeLeft = (endTime - millis()) / 1000;
-
-      lcd.setCursor(1,11);
-      lcd.print(timeLeft);
+  if (limitVal == LOW) {
+    if (mainVal == LOW) {
+      color(green);
+    } else {
+      color(yellow);
     }
 
-    else if (forwardVal == HIGH){
+    if (mainVal == LOW || forwardVal == LOW) {
       stepper.runSpeed();
+    } else if (backVal == LOW) {
+      stepper.setSpeed(-1 * stepSpeed);
+      stepper.runSpeed();
+      stepper.setSpeed(stepSpeed);
     }
+  } else {
+    color(red);
+
+    stepper.setCurrentPosition(0);
     
-    else if (backVal == HIGH){
-      stepper.setSpeed(-1 * speed);
+    if (backVal == LOW) {
+      stepper.setSpeed(-1 * stepSpeed);
       stepper.runSpeed();
-      stepper.setSpeed(speed);
+      stepper.setSpeed(stepSpeed);
     }
+  }
 
-    else{
-      color(yellow)
-    }
-    color(green)
-  }
-  else{
-    color(red)
-    running = 0;
-  }
-  running = 0;
+  float stepsRemaining = stepper.currentPosition();
+
+  int secondsRemaining = int(stepsRemaining / stepSpeed);
+
+  lcd.setCursor(1,11);
+  lcd.print(secondsRemaining);
 }
 
-
-
-void color3(int r, int g, int b]){
-  analogWrite(rp, r);
-  analogWrite(gb, g);
-  analogWrite(bp, b);
-}
-
-void color(int[] color]){
-  analogWrite(rp, color[0]);
-  analogWrite(gb, color[1]);
-  analogWrite(bp, color[2]);
+void color(int c[]){
+  analogWrite(rp, c[0]);
+  analogWrite(gp, c[1]);
+  analogWrite(bp, c[2]);
 }
